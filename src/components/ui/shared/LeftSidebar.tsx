d58ac2895eda5357f7f9ce5e0d44387355ce13c1
Sarah Kwak <sarahkwak79@gmail.com>
2024-01-09 23:54:10 -0500

    leftsidebar profile section

--- src/components/ui/shared/LeftSidebar.tsx
+++ src/components/ui/shared/LeftSidebar.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
 
-export default LeftSidebar
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-2 items-center">
+          <h1 className="text-xl md:text-3xl font-bold">Savllery</h1>
+        </Link>
+
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || "assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+
+        <ul className="flex flex-col gap-6">
+
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default LeftSidebar;
